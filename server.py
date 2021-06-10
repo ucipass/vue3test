6062a3c6d4909478169d28344cf2c26a275b17e4
ucipass@gmail.com <ucipass@gmail.com>
2021-06-10 09:39:41 -0500

    classification change

--- server.py
+++ server.py
@@ -1,83 +1,6 @@
-import queue
-import sys
-import os
-from queue import Queue
-from queue import Empty
-from threading import Thread
-from threading import RLock
-import getopt
 import logging
-import csv
 import argparse
-import asyncio
-import datetime
-import tornado.ioloop
-import tornado.web
-import socketio
-import select
-import datetime
-import time
-import yaml
-import json
-import signal
-signal.signal(signal.SIGINT, signal.default_int_handler)
-
-HTML_PATH = os.path.join( os.getcwd(), "dist")
-
-q_input = Queue()
-q_output = Queue()
-
-sio = socketio.AsyncServer(async_mode='tornado',cors_allowed_origins='*')
-@sio.event
-async def connect(sid, environ):
-    print('connect ', sid)
-    # print(environ)
-@sio.event
-async def disconnect(sid):
-    lastsid = sid
-    print('disconnect ', lastsid)
-@sio.event
-async def data(sid, msg):
-    logging.debug("Received Message %s from %s" %(msg, sid) )
-    q_input.put(msg,True)
-    return "Received Successfully"
-   
-@sio.event
-async def config(sid,msg):
-    logging.debug("Config requested from %s" %(sid) )
-    with open(r'dist/inputs.yaml') as file:
-        json = yaml.load(file, Loader=yaml.FullLoader)
-        return json
-   
-async def start_tornado(port):
-    app = tornado.web.Application([
-        # (r"/submit", MainHandler),
-        (r"/socket.io/", socketio.get_tornado_handler(sio)),
-        (r"/(.*)", tornado.web.StaticFileHandler, {"path": HTML_PATH , "default_filename": "index.html" })
-    ])
-    try:
-        app.listen(port)
-        return True
-    except OSError:
-        logging.critical("Can't listen on port %s" %port)
-        return False
-
-async def send_outputs_from_queue():     
-     logging.debug("Send Sockets Loop Started!")
-     while True:
-        try:
-            json = q_output.get(False)
-            logging.info("Sending event")
-            await sio.emit( "data", json )
-        except Empty:
-            await asyncio.sleep(0.1)   
-
-async def async_services(port):
-    if await start_tornado(port):
-        await asyncio.create_task( send_outputs_from_queue() )
-
-def tornadoThread(param):
-    asyncio.run( async_services(args.port) )
+from webargs import WebArgs
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s UTC %(levelname)s %(module)s(%(funcName)s) [%(process)d-%(thread)d-%(threadName)s]: %(message)s')
 parser = argparse.ArgumentParser()
@@ -86,21 +9,10 @@ parser.add_argument('-p', '--port', help='Specify the tcp port for the web serve
 args = parser.parse_args()
 
 logging.info("Starting server on port %s " % args.port)
-thread = Thread(target = tornadoThread, args = (10, ) , name="TornadoThread")
-thread.daemon = True
-thread.start()
+server = WebArgs(args.port)
+server.run()
 
-while thread.is_alive():
-    try:
-        item=q_input.get(timeout=1) # Timeout is set to avoid blocking ctrl+c
-        output = json.dumps(item, sort_keys=False, indent=4)
-        logging.critical("Pulled item out of queue.")
-        q_output.put("RECEIVED:\n"+output+"\n")
-    except Empty:
-        logging.debug("Nothing in queue to process.")       
-    except KeyboardInterrupt:
-        logging.critical("Keyboard interrupt occurred on thread, quitting main!")
-        sys.exit()
-    except Exception as ex:
-        logging.exception("TASK MAY HAVE BEEN IMPROPERLY HANDLED: " + str(ex) + " ---- " + str(sys.exc_info()[0]))
-        sys.exit()
+while server.is_alive():
+    msg = server.get()
+    if msg:
+        server.put("RECEIVED\n")
---
+++ webargs.py
@@ -0,0 +1,92 @@
+from queue import Queue
+from queue import Empty
+from threading import Thread
+from threading import RLock
+import os
+import logging
+import asyncio
+import tornado.ioloop
+import tornado.web
+import socketio
+import time
+import yaml
+import os
+
+HTML_PATH = os.path.join( os.getcwd(), "dist")
+
+class WebArgs:
+    def __init__(self, port = 8000):
+        self.port = port
+        self.q_input = Queue()
+        self.q_output = Queue()
+        self.sio = socketio.AsyncServer(async_mode='tornado',cors_allowed_origins='*')
+        self.thread = None
+
+    def run(self):
+        self.thread = t1 = Thread(target=self.start_thread2 , name="TornadoThread")
+        self.thread.daemon = True
+        self.thread.start()
+
+    def is_alive(self):
+        return self.thread.is_alive()
+    
+    def get(self):
+        if self.q_input.empty():
+            time.sleep(0.1)
+            return None
+        else:
+            return self.q_input.get()
+
+    def put(self,msg):
+        self.q_output.put(msg)
+    
+    def start_thread2(self):
+        asyncio.run( self.start_thread() )
+
+    async def start_thread(self):
+        self.sio = socketio.AsyncServer(async_mode='tornado',cors_allowed_origins='*')
+        @self.sio.event
+        async def connect(sid, environ):
+            print('connect ', sid)
+            # print(environ)
+        @self.sio.event
+        async def disconnect(sid):
+            lastsid = sid
+            print('disconnect ', lastsid)
+        @self.sio.event
+        async def data(sid, msg):
+            logging.debug("Received Message %s from %s" %(msg, sid) )
+            self.q_input.put(msg,True)
+            return "Received Successfully"
+        @self.sio.event
+        async def config(sid,msg):
+            logging.debug("Config requested from %s" %(sid) )
+            with open(r'dist/inputs.yaml') as file:
+                json = yaml.load(file, Loader=yaml.FullLoader)
+                return json  
+        if await self.start_tornado():
+            await asyncio.create_task( self.send_outputs_from_queue() )
+
+    async def start_tornado(self):
+        app = tornado.web.Application([
+            # (r"/submit", MainHandler),
+            (r"/socket.io/", socketio.get_tornado_handler(self.sio)),
+            (r"/(.*)", tornado.web.StaticFileHandler, {"path": HTML_PATH , "default_filename": "index.html" })
+        ])
+        try:
+            app.listen(self.port)
+            return True
+        except OSError:
+            logging.critical("Can't listen on port %s" %self.port)
+            return False
+
+    async def send_outputs_from_queue(self):     
+        logging.debug("Send Sockets Loop Started!")
+        while True:
+            try:
+                json = self.q_output.get(False)
+                logging.info("Sending event")
+                await self.sio.emit( "data", json )
+            except Empty:
+                await asyncio.sleep(0.1)
+
