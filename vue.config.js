725de805173c4c84d9dbb38748caf35ed17dc60c
ucipass@gmail.com <ucipass@gmail.com>
2021-06-23 12:04:21 -0500

    realtive path added

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,7 @@
 module.exports = {
   configureWebpack: {
     devtool: 'source-map',
-  },
-}
-// module.exports = { 
-//     configureWebpack: { // VS Code debugging 
-//       devtool: 'source-map' 
-//       },  
-//     outputDir: "dist", // Build directory 
-//     publicPath: '' 
-// } 
+    },  
+  outputDir: "dist", // Build directory 
+  publicPath: '' 
+} 
